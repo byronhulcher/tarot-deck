8e04301e49108bbb68894cd185e5ede8ff14e6b5
byronhulcher <byronhulcher@gmail.com>
2016-01-21 16:43:49 -0500

    More tests

--- tests.js
+++ tests.js
@@ -7,3 +7,23 @@ test('Tarot Deck', function(t){
     t.ok(tarot.tarotDeck, "tarot deck exists");
     t.equal(tarot.tarotDeck.length, 78, "tarot deck has 78 cards");
 });
+
+test('Major Arcana', function(t){
+    t.plan(2);
+    t.ok(tarot.majorArcana, "Major Arcana available");
+    t.equal(tarot.majorArcana.length, 22, "Major Arcana has 22 cards");
+});
+
+test('Minor Arcana', function(t){
+    t.plan(2);
+    t.ok(tarot.minorArcana, "Minor Arcana available");
+    t.equal(tarot.minorArcana.length, 56, "Minor Arcana has 56 cards");
+});
+
+test('Suits', function(t){
+    t.plan(6);
+    t.equal(tarot.suits.length, 5, "There exist 5 suits");
+    [ 'major', 'wands', 'cups', 'swords', 'coins' ].forEach(function(suit){
+        t.assert(tarot.suits.indexOf(suit) != -1, "The `" + suit + "` suit is available");
+    });
+});
