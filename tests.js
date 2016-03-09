b5aa95ba27f405faa227c18da7f04f5272ea565c
Byron Hulcher <byronhulcher@gmail.com>
2016-03-09 14:51:10 -0500

    Added tests for drawing a single card and readings

--- tests.js
+++ tests.js
@@ -27,3 +27,18 @@ test('Suits', function(t){
         t.assert(tarot.suits.indexOf(suit) != -1, "The `" + suit + "` suit is available");
     });
 });
+
+test('Draw a single card', function(t){
+    t.plan(1);
+    t.assert(tarot.tarotDeck.indexOf(tarot.drawCard(tarot.tarotDeck) ) != -1, "Able to draw random cards from the deck");
+});
+
+test('Drawing a tarot reading', function(t){
+    t.plan(3);
+    t.assert(tarot.drawReading().length === 3, "Readings default to 3 cards ");
+    
+    var fullReading = tarot.drawReading(tarot.tarotDeck.length);
+    var uniqueReading = [ ...new Set(fullReading) ];
+    t.assert(fullReading.length === uniqueReading.length , "All cards drawn in a reading are unique");
+    t.assert(tarot.drawReading(tarot.tarotDeck.length + 1).length === tarot.tarotDeck.length, "Drawing more cards than are in a deck results in a return equal to deck size")
+});
